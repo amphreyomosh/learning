3201811fb0371135dd7d4f9dfa841f448e4250c9
amphreyomosh <amphreyomosh2001@gmail.com>
2025-02-27 18:07:22 +0300

    added new change

---
+++ schemaTypes/post.js
@@ -0,0 +1,18 @@
+export default {
+    name: "post",
+    title: "Post",
+    type: "document",
+    fields: [
+      {
+        name: "title",
+        title: "Title",
+        type: "string",
+      },
+      {
+        name: "body",
+        title: "Body",
+        type: "text",
+      },
+    ],
+  };
+  
